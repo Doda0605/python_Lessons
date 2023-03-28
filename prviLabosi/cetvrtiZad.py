ab922a9a96403d91f6d4ff1ab4ed4a545598c831
Doda <105274235+Doda0605@users.noreply.github.com>
2023-03-28 21:20:53 +0200

    Update cetvrtiZad.py

--- prviLabosi/cetvrtiZad.py
+++ prviLabosi/cetvrtiZad.py
@@ -1,14 +1,27 @@
+import os
+import sys
 
-file1 = open("mbox.txt", "r")
+f1 = input("Unesite ime 1. datoteke: ")
+f2 = input("Unesite ime 2. datoteke: ")
 
-while(True):
-    # read next line
-    line = file1.readline()
-    # check if line is not null
-    if not line:
-        break
-    # you can access the line
-    print(line.strip())
+def average_spam(f): 
 
-# close file
-file1.close
+    fo = open(os.path.join(sys.path[0], f), "r")
+    brojac = 0
+    total = 0
+
+    for line in fo:
+        if line.startswith("X-DSPAM-Confidence:"):
+            brojac += 1
+
+            #partition vraca tuple od 3 clana, 1. clan je ono prije upisanog stringa, 2. clan je trazeni substring i 3. clan je string nakon trazenog substringa zbog toga pisemo [2] da bi dohvatili zadnji element tupla
+            novi_string = line.partition("X-DSPAM-Confidence: ")[2]
+
+            # pisemo [:-2] da bi makli zadnja 2 charactera u stringu jer nisu brojevi vec \n
+            total += float(novi_string[:-2])
+    
+    print("Ime datoteke:", f)
+    print("Average X-DSPAM-Confidence:", (total/brojac))
+
+average_spam(f1)
+average_spam(f2)
